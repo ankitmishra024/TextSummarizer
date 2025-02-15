1aa6ba75eb89c5c8782b8c572117611faca07c4d
ankitmishra06 <ankitkumar011001@gmail.com>
2025-02-15 10:45:44 +0530

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# TextSummarizer
+# TextSummarizer Using Huggingface
+
+### Workflows
+
+1. config.yaml
+2. Params.yaml
+3. Config entity
+4. Configuration Manager
+5. Update the Components - Data Ingestion, Data Transformation, Model
+6. Create our Pipeline-- Training Pipeline, Prediction Pipeline
+7. Front end -- Api's, Training Api's, Batch Prediction Api
---
+++ src/textSummarizer/utils/common.py
@@ -0,0 +1,40 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from src.textSummarizer.logging import logger
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Reads YAML file and returns a ConfigBox object."""
+    try:
+        with open(path_to_yaml, "r") as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            if content is None:
+                raise ValueError(f"YAML file {path_to_yaml} is empty or incorrectly formatted.")
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("YAML file is empty")
+    except Exception as e:
+        logger.error(f"Error reading YAML file {path_to_yaml}: {e}")
+        raise e
+
+    
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
